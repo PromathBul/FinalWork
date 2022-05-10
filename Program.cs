cd064fc1c484c4eb507475db415e319ac3dcfda7
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-05-10 22:47:04 +0300

    Written method and part of the mail code for displaying of the array

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@
     return arrayForFilling;
 }
 
+void PrintArray(string[] arrayToPrinting)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrinting.Length; i++)
+    {
+        if (i < arrayToPrinting.Length - 1)
+        {
+            Console.Write($"{arrayToPrinting[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{arrayToPrinting[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
 Console.WriteLine("Введите размер одномерного текстового массива:");
 int size = Convert.ToInt32(Console.ReadLine());
 
 string[] originalArray = new string[size];
 originalArray = FillStringArray(originalArray);
+Console.WriteLine("Изначальный массив:");
+PrintArray(originalArray);
