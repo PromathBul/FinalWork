fa015944e2695da81bd76e99d537f9808b11d96c
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-05-10 22:50:07 +0300

    A method's written that solves the task and the main code is added

--- Program.cs
+++ Program.cs
@@ -25,6 +25,29 @@ void PrintArray(string[] arrayToPrinting)
     Console.WriteLine("]");
 }
 
+string[] CreateArrayWithShotStrings(string[] initialArray, int lengthOfElements)
+{
+    int length = new int();
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+        if (initialArray[i].Length <= lengthOfElements)
+        {
+            length++;
+        }
+    }
+    string[] newArray = new string[length];
+    int count = new int();
+    for (int j = 0; j < initialArray.Length; j++)
+    {
+        if (initialArray[j].Length <= lengthOfElements)
+        {
+            newArray[count] = initialArray[j];
+            count++;
+        }
+    }
+    return newArray;
+}
+
 Console.WriteLine("Введите размер одномерного текстового массива:");
 int size = Convert.ToInt32(Console.ReadLine());
 
@@ -32,3 +55,8 @@ string[] originalArray = new string[size];
 originalArray = FillStringArray(originalArray);
 Console.WriteLine("Изначальный массив:");
 PrintArray(originalArray);
+
+int lengthOfElements = 3;
+string[] newArray = CreateArrayWithShotStrings(originalArray, lengthOfElements);
+Console.WriteLine($"Новый массив с длинной элементов не более {lengthOfElements} символов:");
+PrintArray(newArray);
