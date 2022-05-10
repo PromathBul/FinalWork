2257e76944d5768ee6f25414b26bb5e94b154072
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-05-10 22:44:31 +0300

    Written method and part of the main code for filling the initial array by the user

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] FillStringArray(string[] arrayForFilling)
+{
+    for (int i = 0; i < arrayForFilling.Length; i++)
+    {
+        Console.WriteLine($"Введите значение {i + 1}-го элемента:");
+        arrayForFilling[i] = Console.ReadLine();
+    }
+    return arrayForFilling;
+}
+
+Console.WriteLine("Введите размер одномерного текстового массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] originalArray = new string[size];
+originalArray = FillStringArray(originalArray);
